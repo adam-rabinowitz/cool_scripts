0ce5d91a2b7c6171841513a8d8cb393f5fbf8360
Adam Rabinowitz <adam.rabinowitz@embl.de>
2021-11-02 09:16:53 +0100

    formalised a script

--- ps_curve.py
+++ ps_curve.py
@@ -1,9 +1,14 @@
+import argparse
 import cooler
 import cooltools.expected
+import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 
-def get_cvd(clr, name, ignore_diags):
+def get_cvd(path, name, ignore_diags):
+    # Open clr
+    clr = cooler.Cooler(path)
     # Specify regions if they are not supplied
     regions = [
         (chrom, 0, length) for
@@ -28,15 +33,54 @@ def get_cvd(clr, name, ignore_diags):
     return(binned_cvd)
 
 
-clr = cooler.Cooler('/g/furlong/project/90_microC/data/microC/matrices/500/mau2_0h_comb.500_cis_sampled.cool')
-cvd = get_cvd(clr, 'test', 2)
 def create_plot(cvd, palette):
-    plot = sns.lineplot(
-        data=cvd, x='dist.avg', y='balanced.avg', hue='name', palette=('red',)
+    ax = sns.lineplot(
+        data=cvd, x='dist.avg', y='balanced.avg', hue='name', palette=palette
     )
-    plot.set(xlabel='distance', ylabel='probability')
-    plot.set_xscale("log")
-    plot.set_yscale("log")
-    plot.legend().set_title(None)
-    fig = plot.get_figure()
-    fig.savefig('test.png')
+    ax.set(xlabel='distance', ylabel='probability')
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.legend().set_title(None)
+    fig = ax.get_figure()
+    return(fig)
+
+
+# Create argument parser
+parser = argparse.ArgumentParser(
+    'plot ps curves for multiple samples'
+)
+parser.add_argument(
+    '--clrs', required=True, nargs='+', help=('input cooler files. names can be '
+    'supplied via a prefix seprated from the path via a colon. e.g. '
+    'name:/path/to/clr')
+)
+parser.add_argument(
+    '--plot', help='output plot'
+)
+parser.add_argument(
+    '--colors', nargs='+', help='input loop file'
+)
+parser.add_argument(
+    '--ignore_diags', type=int, help='ignore diagonals below this number'
+)
+args = parser.parse_args()
+# Check arguments
+if args.colors is not None:
+    if len(args.colors) != len(args.clrs):
+        raise ValueError('single color must be provided for each clr')
+    colors = ['#' + col for col in args.colors]
+else:
+    colors = None
+# Get names
+names = [x.split(':')[0] for x in args.clrs]
+clrs = [x.split(':')[-1] for x in args.clrs]
+# Read data and concatenate
+cvd_list = [
+   get_cvd(clr, name, args.ignore_diags) for
+   clr, name in zip(clrs, names)
+]
+cvd_df = pd.concat(cvd_list, axis=0, ignore_index=True)
+# Create plot and save
+cvd_plot = create_plot(cvd_df, colors)
+cvd_plot.savefig('test.png', dpi=300)
+
